5f21da2247359c98237d7f1fb0dd84c5f4fa11ce
shuyanglin <shuyanglin.tw@gmail.com>
2024-08-01 14:28:18 +0100

    add create report cutton when it's your holding relationship

---
+++ app/relationship/display/[id]/DisplayRelationship.tsx
@@ -0,0 +1,108 @@
+"use client";
+import styles from "../../../component/Actant.module.css";
+import styles_r from "../../relationship.module.css";
+import Image from "next/image";
+import Navigation from "@/app/component/Navigation";
+import React, { useEffect } from "react";
+import { useParams } from "next/navigation";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { getRelatinoshipbyId } from "@/sanity/sanity-utils";
+import { Actant } from "@/types/Actant";
+import { Agent } from "@/types/Agent";
+
+type DisplayRelationshipProps = {
+  name: string;
+};
+
+const DisplayRelationship: React.FC<DisplayRelationshipProps> = ({ name }) => {
+  const router = useRouter();
+  const params = useParams();
+  const { id } = params;
+  const [relName, setRelName] = useState("");
+  const [rightHolders, setRightHolders] = useState<Actant[]>([]);
+  const [resbonsibilityHolders, setResbonsibilityHolders] = useState<Agent[]>(
+    []
+  );
+  const [rights, setRights] = useState("");
+  const [responsibilities, setResponsibilities] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      const relationship = await getRelatinoshipbyId(String(id));
+      // console.log("relationship: ", relationship);
+      setRelName(relationship.name);
+      setRightHolders(relationship.rightHolder);
+      setResbonsibilityHolders(relationship.responsibilityHolder);
+      setRights(relationship.rights);
+      setResponsibilities(relationship.responsibilities);
+    }
+    fetchData();
+  }, [id]);
+  // console.log("rightHolders: ", rightHolders);
+  return (
+    <>
+      <Navigation
+        left="Back"
+        title="Relationship"
+        right="Log Out"
+        myStyle={{}}
+      ></Navigation>
+      <main className="updateRelationship login">
+        <div className={styles_r.block}>
+          <strong className={styles_r.label}>Relationship Name </strong>
+          <h1>{relName}</h1>
+        </div>
+
+        <div className={styles_r.block}>
+          <strong className={styles_r.label}>Rights Holder </strong>
+          {rightHolders.map((holder) => (
+            <div key={holder.id}>
+              <div>{holder.name}</div>
+              <Image
+                width={45}
+                height={45}
+                priority
+                className={styles.actant_img}
+                alt={`actant image of ${holder.name}`}
+                src={holder.image}
+              />
+            </div>
+          ))}
+        </div>
+
+        <div className={styles_r.block}>
+          <strong className={styles_r.label}>Resbonsibility Holder </strong>
+          {resbonsibilityHolders.map((holder) => (
+            <div key={holder.id}>
+              <div>{holder.name}</div>
+              <Image
+                width={45}
+                height={45}
+                priority
+                className={styles.actant_img}
+                alt={`actant image of ${holder.name}`}
+                src={holder.image}
+              />
+              {name == holder.name && (
+                <form action="">
+                  <button className="button primary">Create Report</button>
+                </form>
+              )}
+            </div>
+          ))}
+        </div>
+        <div className={styles_r.block}>
+          <strong className={styles_r.label}>Rights</strong>
+          <div>{rights}</div>
+        </div>
+        <div className={styles_r.block}>
+          <strong>Responsibilities</strong>
+          <div>{responsibilities}</div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default DisplayRelationship;
--- app/relationship/display/[id]/page.tsx
+++ app/relationship/display/[id]/page.tsx
@@ -1,99 +1,36 @@
-"use client";
-import styles from "../../../component/Actant.module.css";
-import styles_r from "../../relationship.module.css";
-import Image from "next/image";
-import Navigation from "@/app/component/Navigation";
-import React, { useEffect } from "react";
-import { useParams } from "next/navigation";
-import { useState } from "react";
-import { useRouter } from "next/navigation";
-import { getRelatinoshipbyId } from "@/sanity/sanity-utils";
-import { Actant } from "@/types/Actant";
+import DisplayRelationship from "./DisplayRelationship";
+import { options } from "../../../api/auth/[...nextauth]/options";
+import { getAgent } from "@/sanity/sanity-utils";
+import { getServerSession } from "next-auth/next";
 import { Agent } from "@/types/Agent";
+let sessionEmail = "email@email.com";
+let sessionName = "name";
 
-const DisplayRelationship = () => {
-  const router = useRouter();
-  const params = useParams();
-  const { id } = params;
-  const [relName, setRelName] = useState("");
-  const [rightHolders, setRightHolders] = useState<Actant[]>([]);
-  const [resbonsibilityHolders, setResbonsibilityHolders] = useState<Agent[]>(
-    []
-  );
-  const [rights, setRights] = useState("");
-  const [responsibilities, setResponsibilities] = useState("");
+const Page = async () => {
+  const session = await getServerSession(options);
+  console.log(session);
+  if (session) {
+    //retrieve user data through authenticated account
+    sessionEmail = session.user?.email + "";
 
-  useEffect(() => {
-    async function fetchData() {
-      const relationship = await getRelatinoshipbyId(String(id));
-      // console.log("relationship: ", relationship);
-      setRelName(relationship.name);
-      setRightHolders(relationship.rightHolder);
-      setResbonsibilityHolders(relationship.responsibilityHolder);
-      setRights(relationship.rights);
-      setResponsibilities(relationship.responsibilities);
+    // match agent data
+    const agent: Agent = await getAgent(sessionEmail);
+    // //replace user name with coresponding agent name
+    if (agent?.name != undefined) {
+      sessionName = agent.name;
+      // console.log("========agent name: " + agent.name);
     }
-    fetchData();
-  }, [id]);
-  // console.log("rightHolders: ", rightHolders);
+    console.log("session name: ", sessionName);
+  }
   return (
     <>
-      <Navigation
-        left="Back"
-        title="Relationship"
-        right="Log Out"
-        myStyle={{}}
-      ></Navigation>
-      <main className="updateRelationship login">
-        <div className={styles_r.block}>
-          <strong className={styles_r.label}>Relationship Name </strong>
-          <h1>{relName}</h1>
-        </div>
-
-        <div className={styles_r.block}>
-          <strong className={styles_r.label}>Rights Holder </strong>
-          {rightHolders.map((holder) => (
-            <div key={holder.id}>
-              <div>{holder.name}</div>
-              <Image
-                width={45}
-                height={45}
-                priority
-                className={styles.actant_img}
-                alt={`actant image of ${holder.name}`}
-                src={holder.image}
-              />
-            </div>
-          ))}
-        </div>
-
-        <div className={styles_r.block}>
-          <strong className={styles_r.label}>Resbonsibility Holder </strong>
-          {resbonsibilityHolders.map((holder) => (
-            <div key={holder.id}>
-              <div>{holder.name}</div>
-              <Image
-                width={45}
-                height={45}
-                priority
-                className={styles.actant_img}
-                alt={`actant image of ${holder.name}`}
-                src={holder.image}
-              />
-            </div>
-          ))}
-        </div>
-        <div className={styles_r.block}>
-          <strong className={styles_r.label}>Rights</strong>
-          <div>{rights}</div>
-        </div>
-        <div className={styles_r.block}>
-          <strong>Responsibilities</strong>
-          <div>{responsibilities}</div>
-        </div>
-      </main>
+      <DisplayRelationship name={sessionName} />
     </>
   );
 };
 
-export default DisplayRelationship;
+export default Page;
+
+type DisplayRelationshipProps = {
+  name: string;
+};
